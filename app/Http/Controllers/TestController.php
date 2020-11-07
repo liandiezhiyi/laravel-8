9b37b0719b26aff855cd424342c53bc3a60bfd51
tang <3368313239@qq.com>
2020-11-07 20:12:29 +0800

    配置路由

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    //
+    public function index (){
+        return 1111111111111111111;
+    }
+
+    public function test(){
+        return 2222222222222222222;
+    }
+}
